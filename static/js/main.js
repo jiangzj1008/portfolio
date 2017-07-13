573faf3a01493b71edc52b00b7b6970ad28d5655
jiangzj <jiangzj1008@outlook.com>
2017-07-13 23:33:12 +0800

    评论api

--- static/js/main.js
+++ static/js/main.js
@@ -38,27 +38,37 @@ var bindCommentBtn = function() {
         ajax('get', '/api/comment/all', '', function(response) {
             var data = JSON.parse(response)
             console.log(data);
+            insertComment(data)
         })
     })
 }
 
-// 留言板
 var templateComment = function(obj) {
+    var d = new Date(obj.created_time * 1000)
+    var time = d.toLocaleString()
     var t = `
-        <div class="comment-item">
-            <img src="" alt="">
-            <div class="comment-detail">
-                <p class="comment-author">
-                    北顾
-                    <span class="comment-time">2017-07-11, 17:35</span>
-                </p>
-                <p class="comment-content">hello!</p>
-            </div>
+    <div class="comment-item">
+        <img src="" alt="">
+        <div class="comment-detail">
+            <p class="comment-author">
+                ${obj.author}
+                <span class="comment-time">${time}</span>
+            </p>
+            <p class="comment-content">${obj.content}</p>
         </div>
+    </div>
     `
+    return t
 }
-var insertComment = function() {
-
+var insertComment = function(data) {
+    var html
+    for (var i = 0; i < data.length; i++) {
+        var d = data[i]
+        var t = templateComment(d)
+        html += t
+    }
+    var wrap = e('.comment-wrap')
+    wrap.innerHTML = html
 }
 
 bindMainBtn()
